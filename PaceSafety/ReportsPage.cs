0fb9f7fd2bfd10d4564142571a766ce6cfef6ed7
Barak M <Barak@Baraks-MBP.fios-router.home>
2015-12-21 15:04:49 -0500

    basic app skeleton

--- PaceSafety/ReportsPage.cs
+++ PaceSafety/ReportsPage.cs
@@ -11,27 +11,27 @@ namespace PaceSafety
 			// Set Page Title
 			Title = "Reports";
 
-			// Create Screen Elements
-			var list = new ListView {
-				
-			};
-			list.ItemsSource = new string[]{
-				"mono",
-				"monodroid"
-			};
-			list.IsGroupingEnabled = true;
-			list.GroupDisplayBinding = new Binding ("Title");
-			list.GroupShortNameBinding = new Binding ("ShortName");
-
-
-			// Create Screen Content
-			Content = new ContentView {
-				Content = new StackLayout {
-					Children = {
-						list
-					}
-				}
-			};
+//			// Create Screen Elements
+//			var list = new ListView {
+//				
+//			};
+//			list.ItemsSource = new string[]{
+//				"mono",
+//				"monodroid"
+//			};
+//			list.IsGroupingEnabled = true;
+//			list.GroupDisplayBinding = new Binding ("Title");
+//			list.GroupShortNameBinding = new Binding ("ShortName");
+//
+//
+//			// Create Screen Content
+//			Content = new ContentView {
+//				Content = new StackLayout {
+//					Children = {
+//						list
+//					}
+//				}
+//			};
 		}
 
 
